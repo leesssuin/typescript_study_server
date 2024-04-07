166783174e0a8e2a63f86e0f48dda9661d47d07e
leesuin <suin0212@gmail.com>
2024-04-07 21:27:00 +0900

    [#1] setting: mongodb 연동

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const connectMongodb = require("./configs/connectMongodb");
 
 const indexRouter = require("./routes/index");
+
+connectMongodb();
 
 const app = express();
 
