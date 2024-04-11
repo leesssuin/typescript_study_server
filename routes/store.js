5c3af40c85bd15980041f8c15ae885172cd7be3b
leesuin <suin0212@gmail.com>
2024-04-11 16:15:38 +0900

    [#7] feat: store info api 생성

--- routes/store.js
+++ routes/store.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const storeController = require("../controllers/store");
 
 router.get("/", storeController.getStoreList);
+router.get("/:id", storeController.getStoreInfo);
 
 module.exports = router;
