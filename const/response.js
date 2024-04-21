a026a3e6bc1167125625e720fe4d3c4691f0c8a0
leesuin <suin0212@gmail.com>
2024-04-21 19:26:41 +0900

    [#11] feat: 상수처리

---
+++ const/response.js
@@ -0,0 +1,9 @@
+exports.RESULT_MESSAGE = {
+  SUCCESS: "success",
+  FAIL: "fail"
+};
+
+exports.ERROR_MESSAGE = {
+  INVALID_SERVER_ERROR: "Invalid Server Error",
+  VALIDATION_ERROR: "Validation Error"
+};
